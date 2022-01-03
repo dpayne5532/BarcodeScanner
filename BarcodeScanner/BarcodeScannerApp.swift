d07a6cc9815dc784b7913fd096a1b4295dd9d70b
Dan Payne <dpayne5532@gmail.com>
2022-01-03 07:28:11 -0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Dan Payne on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
